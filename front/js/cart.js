66137c3b292820401ab54bb8349aa23f1e408d4e
branye-cyber <105713160+branye-cyber@users.noreply.github.com>
2023-01-25 17:38:29 +0100

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,22 +1,52 @@
-// fonction pour récupérer les données de l'APi
-async function getProductById(product) {
-  const response = await fetch("http://localhost:3000/api/products/" + id);
-  return await response.json();
-}
-console.log(product);
-
 // Récupération des données du localstorage
 const currentCart = JSON.parse(localStorage.getItem("cart")) || [];
-console.log(currentCart);
-function browseLocalStorage() {
-  currentCart.forEach(async (item) => {
-    const product = await getProductById(item.idProduit);
-    basketProduct(product, item.color, item.quantity, product.price);
-    priceTotal(product);
-  });
-}
-console.log(idProduit);
 
+// Boucle pour vérifier le local storage
+for (let j = 0; j < currentCart.length; j++) {
+  fetch("http://localhost:3000/api/products/" + currentCart[j].idProduit)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("Mauvaise réponse du réseau");
+      }
+    })
+    .then(function (product) {
+      console.log(currentCart[j].idProduit);
+      console.log(currentCart[j].color);
+      console.log(currentCart[j].quantity);
+      console.log(product.name);
+      console.log(product.altTxt);
+      console.log(product.price);
+
+      // Récupération de l'élément du DOM qui accueillera les fiches
+      const sectionItems = document.querySelector("#cart__items");
+
+      const articleElement = document.createElement("article");
+      sectionItems.appendChild(articleElement);
+
+      // const cart__itemElement = document.querySelector(".cart__item");
+      // articleElement.appendChild(cart__itemElement);
+
+      // On crée l’élément "img".
+      const imageElement = document.createElement("img");
+      imageElement.src = product.imageUrl;
+      imageElement.alt = product.altTxt;
+      const sectionItem = document.querySelector(".cart__item__img");
+      // sectionItem.appendChild(imageElement);
+
+      const nomElement = document.createElement("h2");
+      nomElement.innerText = product.name;
+      articleElement.appendChild(nomElement);
+
+      const colorElement = document.createElement("p");
+      colorElement.innerText = currentCart[j].color;
+      articleElement.appendChild(colorElement);
+
+      const prixElement = document.createElement("p");
+      prixElement.innerText = `${currentCart[j].quantity * product.price} €`;
+      articleElement.appendChild(prixElement);
+    });
+}
 // Instruction pour vider le localstorage
 // localStorage.clear();
-console.log("Je suis tout en bas de la page 'Panier'");
--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,6 @@
 // Récupération des canapés depuis le fichier JS
 
-fetch(`http://localhost:3000/api/products`)
+fetch("http://localhost:3000/api/products")
   .then(function (response) {
     if (response.ok) {
       return response.json();
@@ -12,40 +12,39 @@ fetch(`http://localhost:3000/api/products`)
     for (let i = 0; i < products.length; i++) {
       // Récupération de l'élément du DOM qui accueillera les fiches
       const sectionItems = document.querySelector(".items");
-      // Création d’une balise dédiée à un canapé
+
       const urlElement = document.createElement("a");
       urlElement.href = "./product.html?id=" + products[i]._id;
-      // Création d’une balise dédiée à un canapé
+      sectionItems.appendChild(urlElement);
+
+      // On crée la balise "article".
       const pieceElement = document.createElement("article");
-      // On crée l’élément img.
+      urlElement.appendChild(pieceElement);
+
+      // On crée l’élément "img".
       const imageElement = document.createElement("img");
       // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
       imageElement.src = products[i].imageUrl;
+      pieceElement.appendChild(imageElement);
+
       // Idem pour le nom, le prix la description et le texte alternatif...
       const nomElement = document.createElement("h3");
       nomElement.innerText = products[i].name;
+      pieceElement.appendChild(nomElement);
+
       const prixElement = document.createElement("price");
       prixElement.innerText = `Prix : ${products[i].price} €`;
+      pieceElement.appendChild(prixElement);
+
       const descriptionElement = document.createElement("p");
       descriptionElement.innerText = products[i].description;
+      pieceElement.appendChild(descriptionElement);
+
       const altTxtElement = document.createElement("p");
       altTxtElement.innerText = products[i].altTxt;
+
       const idElement = document.createElement("p");
       idElement.innerText = products[i]._id;
-
-      // On rattache la balise article à la section Items
-      sectionItems.appendChild(urlElement);
-
-      // On rattache l'url à pieceElement (la balise article)
-      urlElement.appendChild(pieceElement);
-      //a href="./product.html?id=42"
-
-      // On rattache l’image à pieceElement (la balise article)
-      pieceElement.appendChild(imageElement);
-      // Idem pour le nom, le prix la description et le texte alternatif...
-      pieceElement.appendChild(nomElement);
-      pieceElement.appendChild(prixElement);
-      pieceElement.appendChild(descriptionElement);
     }
   })
   .catch(function (error) {
