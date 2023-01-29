d0f42cb34857c8fdc03d926e387905c40e7cb084
branye-cyber <105713160+branye-cyber@users.noreply.github.com>
2023-01-29 15:59:26 +0100

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,41 +12,114 @@ for (let j = 0; j < currentCart.length; j++) {
       }
     })
     .then(function (product) {
-      console.log(currentCart[j].idProduit);
-      console.log(currentCart[j].color);
-      console.log(currentCart[j].quantity);
-      console.log(product.name);
       console.log(product.altTxt);
-      console.log(product.price);
+      console.log(product.name);
+      console.log(currentCart[j].color);
+      console.log(currentCart[j].quantity * product.price + " € (Prix total)");
+      console.log("Qté : " + currentCart[j].quantity);
+      console.log(product.price + " € (Prix unitaire)");
 
       // Récupération de l'élément du DOM qui accueillera les fiches
       const sectionItems = document.querySelector("#cart__items");
 
       const articleElement = document.createElement("article");
+      articleElement.classList.add("cart__item");
       sectionItems.appendChild(articleElement);
 
-      // const cart__itemElement = document.querySelector(".cart__item");
-      // articleElement.appendChild(cart__itemElement);
+      const cart__item__img = document.createElement("div");
+      cart__item__img.classList.add("cart__item__img");
+      articleElement.appendChild(cart__item__img);
 
       // On crée l’élément "img".
       const imageElement = document.createElement("img");
+      // imageElement.classList.add(".cart__item__img");
       imageElement.src = product.imageUrl;
       imageElement.alt = product.altTxt;
-      const sectionItem = document.querySelector(".cart__item__img");
-      // sectionItem.appendChild(imageElement);
+      cart__item__img.appendChild(imageElement);
+
+      const cart__item__content = document.createElement("div");
+      cart__item__content.classList.add("cart__item__content");
+      articleElement.appendChild(cart__item__content);
+
+      const cart__item__content__description = document.createElement("div");
+      cart__item__content__description.classList.add(
+        "cart__item__content__description"
+      );
+      cart__item__content.appendChild(cart__item__content__description);
 
       const nomElement = document.createElement("h2");
       nomElement.innerText = product.name;
-      articleElement.appendChild(nomElement);
+      cart__item__content__description.appendChild(nomElement);
 
       const colorElement = document.createElement("p");
       colorElement.innerText = currentCart[j].color;
-      articleElement.appendChild(colorElement);
+      cart__item__content__description.appendChild(colorElement);
 
       const prixElement = document.createElement("p");
       prixElement.innerText = `${currentCart[j].quantity * product.price} €`;
-      articleElement.appendChild(prixElement);
+      cart__item__content__description.appendChild(prixElement);
+
+      const cart__item__content__settings = document.createElement("div");
+      cart__item__content__settings.classList.add(
+        "cart__item__content__settings"
+      );
+      cart__item__content.appendChild(cart__item__content__settings);
+
+      const cart__item__content__settings__quantity =
+        document.createElement("div");
+      cart__item__content__settings__quantity.classList.add(
+        "cart__item__content__settings__quantity"
+      );
+      cart__item__content__settings.appendChild(
+        cart__item__content__settings__quantity
+      );
+
+      const qtyLabel = document.createElement("p");
+      qtyLabel.innerText = "Qté : ";
+      cart__item__content__settings__quantity.appendChild(qtyLabel);
+
+      const qtyOrdered = document.createElement("input");
+      qtyOrdered.type = "number";
+      qtyOrdered.classList.add("itemQuantity");
+      qtyOrdered.name = "itemQuantity";
+      qtyOrdered.min = 1;
+      qtyOrdered.max = 100;
+      qtyOrdered.value = currentCart[j].quantity;
+      cart__item__content__settings__quantity.appendChild(qtyOrdered);
+
+      const cart__item__content__settings__delete =
+        document.createElement("div");
+      cart__item__content__settings__delete.classList.add(
+        "cart__item__content__settings__delete"
+      );
+      cart__item__content__settings.appendChild(
+        cart__item__content__settings__delete
+      );
+
+      const deleteItem = document.createElement("p");
+      deleteItem.classList.add("deleteItem");
+      deleteItem.innerText = "Supprimer";
+      cart__item__content__settings__delete.appendChild(deleteItem);
+
+      // Récapitulatif du Panier (Nbe articles et Prix total)
+      // Récupération de l'élément du DOM qui accueillera les informations
+      const totalQuantity = document.querySelector("#totalQuantity");
+      for (let j = 0; j < currentCart.length; j++) {
+        let quantityOrdered = 0;
+        quantityOrdered = quantityOrdered + currentCart[j].quantity;
+        console.log(quantityOrdered);
+      }
+
+      // Entrée des coordonnées du Client
+      // Prénom
+      // Nom
+      // Adresse
+      // Ville
+      // Email
+      // Bouton "Commander !"
+      const button = document.querySelector("#order");
+      button.addEventListener("click", (event) => {
+        window.location.href = "confirmation.html";
+      });
     });
 }
-// Instruction pour vider le localstorage
-// localStorage.clear();
--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,2 +1,26 @@
 let variable = "Je suis sur la page 'Confirmation de commande'...";
 console.log(variable);
+
+// Fonction fetch => Obternir les données depuis l'API
+fetch("http://localhost:3000/api/products/")
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log("Mauvaise réponse du réseau");
+    }
+  })
+  // Fonction qui affiche les éléments du DOM
+  .then(function (product) {
+    const orderId = document.querySelector("#orderId");
+
+    orderId.innerText = "65431343444684674";
+
+    // Instruction pour vider le localstorage
+    localStorage.clear();
+  })
+  .catch(function (error) {
+    console.log(
+      "Il y a eu un problème avec l'opération fetch : " + error.message
+    );
+  });
